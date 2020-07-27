9d43956fe643502b1445385337afcc487c4ee96c
Ryan Victory <ryan@beerandraptors.com>
2020-07-27 12:07:07 -0500

    HTML e-mails

--- ruby/log_monitoring/new_dns_queries.rb
+++ ruby/log_monitoring/new_dns_queries.rb
@@ -14,7 +14,7 @@ Thread.new do
         domains = []
         body = "<table border=\"1\"><thead><tr><th>Domain</th><th>Query</th></tr></thead><tbody>"
         new_domains.each do |domain|
-          if domain['msg'] =~ /New domain observed: ([^ ]+) from query (.*)$/
+          if domain['msg'] =~ /New domain observed: ([^ ]+) from query ([^ ]+)$/
             domain = $1.to_s.gsub(".", "[.]")
             query = $2.to_s.gsub(".", "[.]")
             body += "<tr><td>#{domain}</td><td>#{query}</td></tr>"

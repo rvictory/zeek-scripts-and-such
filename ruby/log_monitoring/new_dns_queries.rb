b42cb0c645f1e1ca5d2304ac6540bd33e20a222d
Ryan Victory <ryan@beerandraptors.com>
2020-10-30 20:42:32 -0500

    Added more notices

--- ruby/log_monitoring/new_dns_queries.rb
+++ ruby/log_monitoring/new_dns_queries.rb
@@ -3,6 +3,8 @@ require "json"
 
 mut = Mutex.new
 new_domains = []
+new_user_agents = []
+new_connections = []
 
 emailer = Emailer.new("inbound-smtp.us-east-1.amazonaws.com")
 TIME_BETWEEN_EMAILS = ENV['EMAIL_INTERVAL'].nil? ? 3600 : ENV['EMAIL_INTERVAL'].to_i 
@@ -10,10 +12,12 @@ TIME_BETWEEN_EMAILS = ENV['EMAIL_INTERVAL'].nil? ? 3600 : ENV['EMAIL_INTERVAL'].
 Thread.new do 
   loop do
     mut.synchronize do
+      body = ""
+
       unless new_domains.empty?
         domains = []
         new_domains.uniq! {|x| x["msg"]}
-        body = "<table border=\"1\"><thead><tr><th>Domain</th><th>Query</th><th>Source IP</th><th>Source Name</th></tr></thead><tbody>"
+        body += "<h2>New Domains</h2><table border=\"1\"><thead><tr><th>Domain</th><th>Query</th><th>Source IP</th><th>Source Name</th></tr></thead><tbody>"
         new_domains.each do |domain|
           if domain['msg'] =~ /New domain observed: ([^ ]+) from query ([^ ]+)/
             domain_name = $1.to_s.gsub(".", "[.]")
@@ -26,12 +30,46 @@ Thread.new do
   
         body += "</tbody></table>\n"
   
-        new_domains.each do |domain|
-          body += "<pre>" + JSON.pretty_unparse(domain) + "</pre><hr />"
+        #new_domains.each do |domain|
+        #  body += "<pre>" + JSON.pretty_unparse(domain) + "</pre><hr />"
+        #end
+      end
+
+      unless new_user_agents.empty?
+        body += "<h2>New User-Agents</h2><table border=\"1\"><thead><tr><th>User-Agent</th><th>Host</th><th>Source IP</th><th>Source Name</th></tr></thead><tbody>"
+        new_user_agents.each do |user_agent|
+          if user_agent['msg'] =~ /New User-Agent: (.+) connecting to host (.+)/
+            ua = $1.to_s
+            host = $2.to_s
+            body += "<tr><td>#{ua}</td><td>#{host}</td><td>#{user_agent["id.orig_h"]}</td><td>#{user_agent['host_name']}</td></tr>"
+          else
+            body += user_agent['msg'] + "\n"
+          end
         end
-        emailer.send_alert_email("New Domains Observed (#{new_domains.length} domains)", body, "zeek@raptormail.net", "Zeek", "rvictory@raptormail.net", "Ryan Victory")
+        body += "</tbody></table>\n"
+      end
+
+      unless new_connections.empty?
+        body += "<h2>New Connection Pairs</h2><table border=\"1\"><thead><tr><th>Orig_h</th><th>Resp_h</th><th>Resp_p</th><th>Source Name</th></tr></thead><tbody>"
+        new_connections.each do |connection|
+          if connection['msg'] =~ /New Orig_h Resp_h Pair: (.+)/
+            parts = $1.to_s.split(" -> ")
+            orig_h = parts[0]
+            resp_h = parts[1]
+            body += "<tr><td>#{orig_h}</td><td>#{resp_h}</td><td>#{connection["id.resp_p"]}</td><td>#{connection['host_name']}</td></tr>"
+          else
+            body += connection['msg'] + "\n"
+          end
+        end
+        body += "</tbody></table>\n"
+      end
+
+      if body != ""
+        emailer.send_alert_email("New Things Observed (#{new_domains.length} domains)", body, "zeek@raptormail.net", "Zeek", "rvictory@raptormail.net", "Ryan Victory")
       end
       new_domains = []
+      new_user_agents = []
+      new_connections = []
     end
     sleep TIME_BETWEEN_EMAILS
   end
@@ -44,8 +82,7 @@ STDIN.each_line do |line|
   rescue
     next
   end
-  next unless data["note"] == "DNSMonitor::DNS_New_FQDN"
-  next if data["id.orig_h"] == "192.168.3.143"
+
   # Try to grab the name of the system from the current day's dhcp logs
   source_ip = data["id.orig_h"]
   system_name = "<unknown>"
@@ -62,8 +99,21 @@ STDIN.each_line do |line|
 
   data["host_name"] = system_name
 
-  puts "Queued #{data['msg']} from source host #{system_name}"
-  mut.synchronize do
-    new_domains.push(data)
+  if data["note"] == "DNSMonitor::DNS_New_FQDN"
+    next if data["id.orig_h"] == "192.168.3.143"
+    puts "Queued #{data['msg']} from source host #{system_name}"
+    mut.synchronize do
+      new_domains.push(data)
+    end
+  elsif data["note"] == "NewConnectionMonitor::NewOrigRespPair"
+    puts "Queued #{data['msg']} from source host #{system_name}"
+    mut.synchronize do
+      new_connections.push(data)
+    end
+  elsif data["note"] == "NewUserAgentMonitor::NewUserAgent"
+    puts "Queued #{data['msg']} from source host #{system_name}"
+    mut.synchronize do
+      new_user_agents.push(data)
+    end
   end
 end
